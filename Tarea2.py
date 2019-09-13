602d97a8d09ad9c8745a432e0cbb69fda4f656d5
JervisNW <44644064+JervisNW@users.noreply.github.com>
2019-09-13 15:55:53 -0600

    Add files via upload
    segundo commit

---
+++ Tarea2.py
@@ -0,0 +1,12 @@
+def Tarea2(A,B):
+    try:
+        if A>=B:
+            return(int(A)+int(B), int(A)-int(B),int(A)*int(B))
+        else:
+            return(-2)
+    except:
+        return(-2)
+
+F=Tarea2(3,"f")
+print(F)
+            
---
+++ test_sample.py
@@ -0,0 +1,11 @@
+def Tarea2(A,B):
+    try:
+        if A>=B:
+            return([int(A)+int(B), int(A)-int(B),int(A)*int(B)])
+        else:
+            return(-2)
+    except:
+        return(-2)
+  
+def test_answer():
+    assert Tarea2(5,10)==-2
